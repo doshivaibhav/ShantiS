4abfa345540643b22191e5a9d2d09214ecccbe17
Badal Davda <badal.davda@somaiya.edu>
2016-06-04 13:47:02 +0530

    Updation Changes to all forms after Job

--- client/jobs/docDocks.js
+++ client/jobs/docDocks.js
@@ -4,3 +4,20 @@ self.autorun(function(){
 	self.subscribe('docDocks');
 });
 });
+Template.docDocks.helpers({
+	docksMode:function(){
+		var id = FlowRouter.getParam('id');
+		var id1 = DocDocks.findOne({JobId:id});
+		//console.log(id1);
+		if(typeof id1 == 'undefined')
+			return true;
+		else
+			return false;
+	},
+	docksId:function(){
+		var id = FlowRouter.getParam('id');
+		id1 = DocDocks.findOne({JobId:id});
+		//console.log(id1._id)
+		return id1;
+	},
+});
